4bedaac2a6390aef6d65f12d3c94912a85b481d6
nam956287 <86147796+nam956287@users.noreply.github.com>
2021-08-02 12:11:12 +0700

    Add files via upload

---
+++ src/main/java/com/edu/InternJavaBaitestApplication.java
@@ -0,0 +1,17 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.edu.model.User;
+
+@SpringBootApplication
+public class InternJavaBaitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternJavaBaitestApplication.class, args);
+	}
+	
+	
+}
